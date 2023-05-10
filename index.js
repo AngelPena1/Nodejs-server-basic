f3254d39b0bcbb7911ecfb6cfa93143e88a87978
AngelPS <1095357@est.intec.edu.do>
2023-05-09 21:39:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 100000,
+  })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json('Hola Mundo')
+})
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server running on port ${process.env.PORT || 8000}`);
+});
+
